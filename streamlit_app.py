f88eb376c2318f123449e623507966d274a7ed1c
vgerganna <107188377+vgerganna@users.noreply.github.com>
2022-06-13 12:05:56 +0300

    Update streamlit_app.py
    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/watermelon
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruitivice_normalized)
+streamlit.dataframe(fruityvice_normalized)
